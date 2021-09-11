14f5e1029f76282c5b5b1d43155a7c92f3196bc1
Ahmed Mikkawe <dev.ahmedmikkawe@gmail.com>
2021-09-11 22:08:05 +0200

    Finish

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 let shareBtn = document.querySelector(".share");
 let shareToolTip = document.querySelector(".share-tool-tip");
+let authorComponent = document.querySelector(".author-component");
+let authorInfo = document.querySelector(".author-info");
+let shareMobileView = document.querySelector(".share-mobile-view");
 shareBtn.addEventListener('click',toggleClick);
 function toggleClick() {
-    if(shareToolTip.style.display === 'flex'){
+    if(window.innerWidth >= 426 ){
+        if(shareToolTip.style.display === 'flex'){
+            shareToolTip.style.display = 'none';
+        }else{
+            shareToolTip.style.display = 'flex';
+        }
+    }
+    if(window.innerWidth <= 425){
         shareToolTip.style.display = 'none';
-    }else{
-    shareToolTip.style.display = 'flex';
+        authorComponent.classList.toggle('black');
+        authorInfo.classList.toggle('d-none');
+        shareMobileView.classList.toggle('show');
     }
 }
